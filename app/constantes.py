06092c048c129adf74ee868799818879f145da4d
dalek63 <thesupremedalek63@gmail.com>
2023-06-02 14:13:08 +0200

    seeder fait + ajout fichier constantes

---
+++ app/constantes.py
@@ -0,0 +1,7 @@
+connect = 'mongodb+srv://dalek63:techno@technolog.ec8jwtk.mongodb.net/'
+
+db = "MagasinBurger"
+
+collection1 = "Burgers"
+
+collection2 = "Evaluateurs"
---
+++ app/seeder.py
@@ -0,0 +1,28 @@
+
+from pymongo import MongoClient
+
+from constantes import *
+
+
+client = MongoClient(connect)
+Magasin = client[db]
+burgers= Magasin[collection1]
+evaluateurs = Magasin[collection2]
+
+b1 = { "prix" : 10, "description" : 'Burger1' , "allegènes" : ["moutarde"] , "cuisson" : "saignant", "scoville": 5000}
+b2 = { "prix" : 15, "description" : 'Burger2' , "allegènes" : ["citron","noix"] , "cuisson" : "cuit", "scoville": 50000}
+b3 = { "prix" : 19, "description" : 'Burger3' , "allegènes" : ["cacao","noix"] , "cuisson" : "à point", "scoville": 15000}
+
+
+
+peuple_burgers = [b1,b2,b3]
+
+peuple_evaluateurs = [
+    { "date" : "10/10/2010", "burgers" : [b1,b1,b2]},
+    { "date" : "11/10/2010", "burgers" : [b1,b1,b2,b2]},
+    { "date" : "12/10/2010", "burgers" : [b3,b3]}
+]
+
+burgers.insert_many(peuple_burgers)
+
+evaluateurs.insert_many(peuple_evaluateurs)
